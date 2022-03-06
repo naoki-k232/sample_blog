37751c1b2ecbc9eea0b3d32957729934a892fd59
naoki-k <good123456305@gmail.com>
2022-03-06 20:23:39 +0900

    ブログテーブル作成

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BlogFactory extends Factory
+{
+    protected $model = Blog::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(20),
+            'body'  => $this->faker->realText(100),
+        ];
+    }
+}
