f1033446867222bb46f49fe5c61df862ce4a91b6
naoki-k <good123456305@gmail.com>
2022-03-06 20:53:00 +0900

    ブログタイトルの一覧表示

--- tests/Feature/Controllers/BlogViewControllerTest.php
+++ tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,20 +2,30 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class BolgViewControllerTest extends TestCase
+class BlogViewControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test method */
     function ブログのトップページを開ける()
     {
 
         // $this->withoutExceptionHandling();
 
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+
         $response = $this->get('/');
 
         $response->assertOk();
+
+        $response->assertSee($blog1->title);
+        $response->assertSee($blog2->title);
+        $response->assertSee($blog3->title);
     }
 }
